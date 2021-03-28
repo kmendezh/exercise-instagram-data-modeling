6d2a9fe042577c4cc85bf5cd57e3bc637c809a9d
kmendezh <kendallmh2@hotmail.com>
2021-03-28 01:18:51 +0000

    Instagram Data Modeling - K.Mendez

--- src/models.py
+++ src/models.py
@@ -8,26 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Link(Base):
+   __tablename__ = 'Post_and_Post_Media_Relationship'
+   post_id = Column(Integer, ForeignKey('post.id'), primary_key = True)
+   post_media_id = Column(Integer, ForeignKey('post_media.id'), primary_key = True)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key = True)
+    comment_Text = Column(String(250), ForeignKey('comments.id'))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    likes = Column(Integer)
+
+    # Post -> Comments: One to Many relationship
+    comments = relationship("Comments")
+
+    # Post ID -> Post Media ID: Many to many relationship
+    post_media_id = relationship('Post_Media', secondary = 'Post_and_Post_Media_Relationship')
+
+class Post_Media(Base):
+    __tablename__ = "post_media"
+    id = Column(Integer, primary_key = True)
+    media_Type = Column(String(20))
+    url = Column(String(100), nullable = False)
+    # Post Media ID -> Post ID: Many to many relationship
+    post_id = relationship('Post', secondary = 'Post_and_Post_Media_Relationship')
+
+class Comments(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key = True)
+    user_Name = Column(String(20), nullable = False)
+    profile_Description = Column(String(100))
+    pswd = Column(String(20), nullable = False)
+    email = Column(String(30), nullable = False)
+
+    #User ID -> post: One to Many relationship
+    post = relationship(Post)
+
+    #User ID -> comments: One to Many relationship
+    comments = relationship(Comments)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
